0f03a98cc54d3d80e1a30807c078062216284155
Evgeny Zhernoklev <John@Mac-mini-Evgeny-2.local>
2017-03-08 13:54:19 +0300

    html, icons, css-skeleton added

--- src/js/common.js
+++ src/js/common.js
@@ -2,5 +2,41 @@ $(document).ready(function() {
     var $window = $(window),
         $body = $('body');
 
+    /*************** check if browser supports svg ***************/
+    var supportsSVG = !!document.createElementNS && !!document.createElementNS("http://www.w3.org/2000/svg", "svg").createSVGRect;
+    if (!supportsSVG) {
+        $body.addClass('no-svg');
+    }
+
+
+
+    /*************** header scroll ***************/
+    // var $topMenu = $('.header_top'),
+    //     $bottomMenu = $('.header_bottom'),
+    //     topMenuHeight = $topMenu.height(),
+    //     scrollTop = 0;
+    //
+    // function headerScroll() {
+    //     if (scrollTop > topMenuHeight) {
+    //         $bottomMenu.addClass('is-fixed');
+    //         $body.addClass('menu-fixed');
+    //     } else {
+    //         $bottomMenu.removeClass('is-fixed');
+    //         $body.removeClass('menu-fixed');
+    //     }
+    // }
+    //
+    // $window.on('load scroll', function() {
+    //     topMenuHeight = $topMenu.height();
+    //     scrollTop = $window.scrollTop();
+    //     headerScroll();
+    // });
+
+
+
+    /*************** toggle blocks ***************/
+
+
+
     
 });
