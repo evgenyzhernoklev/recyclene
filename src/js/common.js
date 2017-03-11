dc21c94d6841d6499652f59733f6e4ca85045f84
Evgeny Zhernoklev <John@Mac-mini-Evgeny-2.local>
2017-03-11 15:19:10 +0300

    postavshik register - login_1 page added

--- src/js/common.js
+++ src/js/common.js
@@ -10,90 +10,89 @@ $(document).ready(function() {
 
 
 
-    /*************** menu ***************/
-    var $topMenu = $('.header-scroll'),
-        topMenuHeight = $topMenu.height(),
-        topMenuPosition = $topMenu.offset().top,
-        scrollTop = 0;
-
-    $window.on('load scroll', function() {
-      scrollTop = $window.scrollTop();
-
-      if (scrollTop > topMenuPosition) {
-        $topMenu.addClass('is-fixed');
-      } else {
-        $topMenu.removeClass('is-fixed');
-      }
-    });
-
-
-
-    var $menuLinks = $('.link-scroll'),
-        $contentToScroll = $('.content-scroll');
-
-    $menuLinks.on('click', function (e) {
-      e.preventDefault();
-      var targetBlockIndex = +$(this).data('block'),
-          targetBlockTopPosition = $contentToScroll.eq(targetBlockIndex).offset().top;
-
-      $menuLinks.removeClass('is-active');
-      $(this).addClass('is-active');
-
-      $('html, body').animate({
-        scrollTop: targetBlockTopPosition - topMenuHeight
-      }, 1500);
-    });
-
-
-
-    /*************** toggle blocks ***************/
-    $('.tabs-link').on('click', function(e) {
-      e.preventDefault();
-      if ( $(this).hasClass('is-active') ) {
-        return;
-      }
-
-      var $parent = $(this).closest('.tabs-wrapper'),
-          $content = $parent.find('.tabs-content'),
-          targetBlockIndex = +$(this).data('block'),
-          $targetBlock = $content.eq(targetBlockIndex);
-
-      $parent.find('.tabs-link').removeClass('is-active');
-      $(this).addClass('is-active');
-
-      $content.removeClass('is-active');
-      $targetBlock.addClass('is-active');
-    });
-
-
-
-    /*************** slider who sells ***************/
-    $('.sliderSells').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      prevArrow: '<span class="icon icon--sliderLeft"></span>',
-      nextArrow: '<span class="icon icon--sliderRight"></span>'
-    });
 
 
 
 
-    /*************** slider how it works ***************/
-    $('.slide-arrow-prev').on('click', function(e) {
-      e.preventDefault();
-      $(this).closest('.slick-slider').slick('slickPrev');
-    });
 
-    $('.slide-arrow-next').on('click', function(e) {
-      e.preventDefault();
-      $(this).closest('.slick-slider').slick('slickNext');
-    });
+    /*************** js for landing ***************/
+    if ( $('.sliderSells').length ) {
+      /*************** menu ***************/
+      var $topMenu = $('.header-scroll'),
+          topMenuHeight = $topMenu.height(),
+          topMenuPosition = $topMenu.offset().top,
+          scrollTop = 0;
 
-    $('.howSliderLp').slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      fade: true,
-      prevArrow: '',
-      nextArrow: ''
-    });
+      $window.on('load scroll', function() {
+        scrollTop = $window.scrollTop();
+
+        if (scrollTop > topMenuPosition) {
+          $topMenu.addClass('is-fixed');
+        } else {
+          $topMenu.removeClass('is-fixed');
+        }
+      });
+
+      var $menuLinks = $('.link-scroll'),
+          $contentToScroll = $('.content-scroll');
+
+      $menuLinks.on('click', function (e) {
+        e.preventDefault();
+        var targetBlockIndex = +$(this).data('block'),
+            targetBlockTopPosition = $contentToScroll.eq(targetBlockIndex).offset().top;
+
+        $menuLinks.removeClass('is-active');
+        $(this).addClass('is-active');
+
+        $('html, body').animate({
+          scrollTop: targetBlockTopPosition - topMenuHeight
+        }, 1500);
+      });
+
+      /*************** toggle blocks ***************/
+      $('.tabs-link').on('click', function(e) {
+        e.preventDefault();
+        if ( $(this).hasClass('is-active') ) {
+          return;
+        }
+
+        var $parent = $(this).closest('.tabs-wrapper'),
+            $content = $parent.find('.tabs-content'),
+            targetBlockIndex = +$(this).data('block'),
+            $targetBlock = $content.eq(targetBlockIndex);
+
+        $parent.find('.tabs-link').removeClass('is-active');
+        $(this).addClass('is-active');
+
+        $content.removeClass('is-active');
+        $targetBlock.addClass('is-active');
+      });
+
+      /*************** slider who sells ***************/
+      $('.sliderSells').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<span class="icon icon--sliderLeft"></span>',
+        nextArrow: '<span class="icon icon--sliderRight"></span>'
+      });
+
+      /*************** slider how it works ***************/
+      $('.slide-arrow-prev').on('click', function(e) {
+        e.preventDefault();
+        $(this).closest('.slick-slider').slick('slickPrev');
+      });
+
+      $('.slide-arrow-next').on('click', function(e) {
+        e.preventDefault();
+        $(this).closest('.slick-slider').slick('slickNext');
+      });
+
+      $('.howSliderLp').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        prevArrow: '',
+        nextArrow: ''
+      });
+    }
 });
